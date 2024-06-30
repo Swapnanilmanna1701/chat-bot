629a81dfef0b2b36e324365aee9a8a7af4e02128
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-06-30 21:40:18 +0530

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  function generateAnswer (){
+    const response = await axios({
+      url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyCtA0qBONuzYWCCuknfCw-gPKYnr_H1UA8",
+      method: "POST",
+      data: {
+        contents:[{"parts":[{"text":"Explain how AI works"}]}]
+        
+      }
+      
+    })
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Chat Bot</h1>
     </>
   )
 }
