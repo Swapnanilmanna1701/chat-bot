a24807f843cfef0972318977368d5e9c710c5513
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-07-01 12:19:47 +0530

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,26 @@ import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  function generateAnswer (){
+  const [question, setQuestion] = useState("");
+  async function generateAnswer (){
+    console.log("loading...")
     const response = await axios({
-      url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyCtA0qBONuzYWCCuknfCw-gPKYnr_H1UA8",
+      url:`process.env.GEMINI_URL`,
       method: "POST",
       data: {
         contents:[{"parts":[{"text":"Explain how AI works"}]}]
         
       }
       
-    })
+    });
+    console.log(response['data']['candidates'][0]['content']['parts'][0]['text']);
 
   }
 
   return (
     <>
       <h1>Chat Bot</h1>
+      <button onClick={generateAnswer}>Generate Answer</button>
     </>
   )
 }
